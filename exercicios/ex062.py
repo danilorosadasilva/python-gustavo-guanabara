7ee072bb43357053e6fa05de0180482ee56e4125
Danilo Rosa da Silva <danilorosadasilva56@gmail.com>
2024-01-22 14:20:22 -0300

    atualizaçao

--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -1,11 +1,16 @@
 # Progressao aritimetica
 t1 = int(input('primeiro termo: '))
-rasao = int(input('rasao: '))
-termos = int(input('Quantidade de termos: '))
+rasao = int(input('rasao:'))
 cont = 0
 resut = 0
-while cont != termos +1:
-    cont += 1
-    resut = rasao + (t1 * (cont-1))
-    print(resut, end=' => ')
-print('fim')
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont != total:
+        cont += 1
+        resut = rasao + (t1 * (cont-1))
+        print(resut, end=' => ')
+    print('pausa')
+    mais = int(input('Quantos termos voce quer mais? '))
+print('Progressao finalizada com {} termos'.format(total))
