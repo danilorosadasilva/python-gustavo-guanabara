28a0be07283b0bf70e6f52b48748bab283940129
Danilo Rosa da Silva <danilorosadasilva56@gmail.com>
2024-01-28 09:53:42 -0300

    at
    at

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -1,4 +1,11 @@
-n = ()
-for n in range(2):
-    n = int(input('Digite o numero: '))
-print(n)
+núm = (int(input('Digite um numero: ')),
+     int(input('Digite outro numero: ')),
+     int(input('Digite mais um numero: ')),
+     int(input('Digite outro de novo numero: ')),
+     int(input('Digite ultimo numero: ')),)
+print(f'O 9 apareceu {núm.count(9)} posiçao')
+print(f'Posiçao do 3 {núm.index(3)+1}')
+print('Os numeros pares sao ',end='')
+for i in núm:
+    if i % 2 ==0:
+        print(i, end=' ')
