0284ff63717473c843f2c66ac5ff161cd111c998
Danilo Rosa da Silva <danilorosadasilva56@gmail.com>
2024-01-28 11:56:29 -0300

    at
    at

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -3,8 +3,14 @@ núm = (int(input('Digite um numero: ')),
      int(input('Digite mais um numero: ')),
      int(input('Digite outro de novo numero: ')),
      int(input('Digite ultimo numero: ')),)
-print(f'O 9 apareceu {núm.count(9)} posiçao')
-print(f'Posiçao do 3 {núm.index(3)+1}')
+if 9 in núm:
+    print(f'O 9 apareceu {núm.count(9)} posiçao')
+else:
+    print('O valor 9 não foi digitado')
+if 3 in núm:
+    print(f'Posiçao do 3 é a {núm.index(3)+1}')
+else:
+    print('O valor 3 nao foi digitado')
 print('Os numeros pares sao ',end='')
 for i in núm:
     if i % 2 ==0:
--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -1,3 +1,17 @@
-tuplas = ('Pao', 2.50, 'queijo', 5.20, 'carne', 20.00)
-for i in tuplas:
-    print(i)
+print('_|'*42)
+print('_|'*42)
+print(f'{'MERCADO-DO-DANILO':-^42}')
+setup = ('Teclado Gamer', 50,
+        'Mouse Gamer', 70,
+        'Controle', 2.50,
+        'Monitores', 700,
+        'Webcam', 300,
+        'Headset', 800,
+        'Mesa Gamer', 1986,
+        'Cadeira Gamer',520)
+for iten in range(len(setup)):
+    if iten % 2 == 0:
+        print(f'{setup[iten]:.<30}', end='')
+    else:
+        print(f'{setup[iten]:>7.2f}R$')
+print(f'{'':-^42}')
