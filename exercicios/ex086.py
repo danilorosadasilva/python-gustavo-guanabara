6ba2ecd431739f7388bc365fe75f8d37b6bc9451
Danilo Rosa da Silva <danilorosadasilva56@gmail.com>
2024-03-07 15:31:36 -0300

    at

--- exercicios/ex086.py
+++ exercicios/ex086.py
@@ -1,12 +1,10 @@
-lista_das_coisas = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]]
-lista = [[],[],[]]
-count = 0
-for ind,i in enumerate(lista_das_coisas):
-    if ind % 3 == 0 and ind != 0:
-        count += 1
-    lista[count].append(int(input(f'Dijite um valor para {i}: ')))
+matris = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
+for l in range(0, 3):
+    for c in range(0, 3):
+        matris[l][c] = int(input(f'Valor para [{l}:{c}]: '))
 
-print('-'*30)
-for i in lista:
-    print(f'[ {i[0]} ] [ {i[1]} ] [ {i[2]} ]')
+for i in range(0, 3):
+    for c in range(0, 3):
+        print(f'[ {matris[i][c]:^5} ]', end='')
+    print()
